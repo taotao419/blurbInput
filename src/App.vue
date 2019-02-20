99f397f5636156d77c2d0317ab48af4245b1562d
Carrier.Zhang <carrier.zhang@ef.com>
2019-02-20 13:23:39 +0800

    Add element UI component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,24 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <el-checkbox v-model="checked">Check Or Not</el-checkbox>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      checked: true
+    };
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
